dc704116ece645c6faa7a4bcf76b5b57081c8d9c
Victor Bäckman <victorbackman2@gmail.com>
2017-11-08 18:15:44 +0100

    fixed api main

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
-import graph, ant_colony_optimization as aco
-import numpy as np
-from joblib import Parallel, delayed
+from api import API
+API_KEY = '9b2a9819-b49b-41c0-8692-149ea06dfe58'
+_api = API(API_KEY)
 
-# Set constants
-nbr_cities = 10
-nbr_transport_types = 5     # 0: bicycle, 1: car, 2:buss, 3: boat, 4: plane
-nbr_colonies = 8
-nbr_parallel_jobs = 4
 
-# Init map
-city_locations = graph.generate_random_locations(nbr_cities)
-distance_matrix = graph.create_distance_matrix(city_locations)
-city_extra_points = graph.generate_random_city_extra_points(nbr_cities, max_point=1)
+def solve(game):
 
-# Create graph
-static_connection_graph = graph.generate_static_connection_graph(nbr_transport_types, nbr_cities)
-punishment_graph = graph.add_random_paths_to_static_graph(distance_matrix, static_connection_graph)
-punishment_matrix, transport_matrix = graph.merge_graph_to_matrix(punishment_graph)
+    # --- Available commands ---
+    # TRAVEL [NORTH|SOUTH|WEST|EAST]
+    # [BUS|TRAIN|FLIGHT] {CityName}
+    # SET_PRIMARY_TRANSPORTATION [CAR|BIKE]
 
-print(f'{np.count_nonzero(~np.isnan(punishment_matrix))} / {nbr_cities*nbr_cities} connections')
-#travel_time_graph, punishment_graph, score_graph = graph.generate_punishment_graph_from_distance(nbr_transport_types, distance_matrix, city_extra_points)
+    # TODO: Implement your solution
 
-# Set arguments
-args = (punishment_matrix, transport_matrix, city_extra_points)
-kwargs = {
-    'start_city': 2,
-    'target_city': 9,
-    'nbr_ants': 50,
-    'verbose': True,
-    'evaporation': 0.5,
-    'alpha': 1.0,   # pheromones
-    'beta': 3.0     # scores
-}
+    # Example solution
+    solution = list()
+    x = game.start.x
+    y = game.start.y
+    while x < game.end.x:
+        x += 1
+        solution.append("TRAVEL EAST")
+    while y < game.end.y:
+        y += 1
+        solution.append("TRAVEL SOUTH")
 
-# Run colonies in parallel
-best_path, best_score, all_results = aco.run_parallel_colonies(nbr_parallel_jobs, nbr_colonies, args, kwargs)
+    return solution
 
-# Run 1 colony
-#best_path, best_score = aco.summon_the_ergodic_colony(*args, **kwargs)
 
-# Plot the graph
-graph.plot_graph(city_locations, punishment_graph, best_path)
+def main():
+    _api.initGame()
+    game = _api.getMyLastGame()
+    #Or get by gameId:
+    #game = _api.getGame()
+    solution = solve(game)
+    _api.submitSolution(solution, game.id)
+
+    main()
